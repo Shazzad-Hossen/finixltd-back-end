d64ba6a7fafe48b7c60d63e4f1ee7758e212a855
Shazzad Hossen <tipsfire.com@gmail.com>
2026-04-13 23:42:34 +0600

    all routes of fetching blogs is now free from auth middleware

--- src/services/blog/blog.js
+++ src/services/blog/blog.js
@@ -6,8 +6,8 @@ const { createBlog, getAllBlogs, getSingleBlog, patchBlog, deleteBlog } = requir
 function blog(){
     
     this.route.post('/blog', auth,createBlog(this));
-    this.route.get('/blogs', auth, getAllBlogs(this));
-    this.route.get('/blog/:id', auth, getSingleBlog(this));
+    this.route.get('/blogs', getAllBlogs(this));
+    this.route.get('/blog/:id', getSingleBlog(this));
     this.route.patch('/blog/:id', auth, patchBlog(this));
     this.route.delete('/blog/:id', auth, deleteBlog(this));
 
